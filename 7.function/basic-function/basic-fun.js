67ad73a8022f08312a4e7d6a5efb550ac511dfa6
srushti <srushtikathiriya2003@gmail.com>
2024-02-14 08:27:02 +0530

    14-02-2024

---
+++ 7.function/basic-function/basic-fun.js
@@ -0,0 +1,39 @@
+// function  -> perform to specific task
+
+// hosting
+// square('10','5');     // function call
+
+// defination
+// function square(a,b){
+    // console.log(a*b);
+// }
+
+
+// basic function
+// function add(a,b){
+//     return a + b;
+// }
+// // console.log(add(5,15));
+// let x = add(10,15);
+// console.log(x);
+
+
+// anonymous function / function as expression
+// let product = function(x , y){
+//     return x * y;
+// }
+// console.log(product(11,2));
+
+
+//IIFE
+
+// (function hello()
+// {
+//     console.log("Hello world");
+// })();
+
+// let x = (function hello(a,b)
+// {
+//     return (a - b);
+// })("10" , 15);
+// console.log(x);

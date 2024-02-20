e62bdcf6a14334f1f927bc31d3aacb84096f0a1b
srushti <srushtikathiriya2003@gmail.com>
2024-02-20 08:54:34 +0530

    20-02-2024

--- 7.function/basic-function/basic-fun.js
+++ 7.function/basic-function/basic-fun.js
@@ -57,3 +57,64 @@
 // function constructer
 // let let hello = new Function("a" , "b" ,"return a * b");
 // console.log(hello(12 , 10));
+
+
+// Function are Object:-
+// function hello(a,b,c){
+//     return arguments.length;
+// }
+// console.log(hello(1,2,3));
+// console.log(hello(1,2,3).toString());
+
+
+// Function Rest Parameters:-
+
+// function add(...args) {
+//     let sum = 0;
+// for (let i=0; i<args.length;i++) 
+//                 sum = sum + args[i];
+//     return sum;
+// };
+
+
+// Arguments Object:-
+
+// function add() {
+//     let sum = 0;
+//     for (let i = 0; i < arguments.length; i++) {
+//         sum += arguments[i];
+//     }
+//     return sum;
+// };
+// console.log(add(1, 123, 500, 115, 44, 88));   // 871
+
+
+// Generator Function:-
+// function* generate() {
+//     yield 1;
+//     yield 2;
+//     yield 3;
+//     return 4;
+// }
+
+// let generator = generate();
+// console.log(generator.next().value); // 1
+// console.log(generator.next().value); // 2
+// console.log(generator.next().value); // 3
+// console.log(generator.next().value); // 4
+
+
+// this Keyword:-
+
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 24,
+    hello: function() {
+        let data = this.firstName + this.lastName;
+        return data;
+                    // return this.firstName + " " + this.lastName;
+        }
+};   // John Doe
+console.log(person.hello());
+		
